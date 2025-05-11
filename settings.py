c5bd944ac690d293a3323b9fe3a4d50ab6cb49b6
Terraron <didudjdjh73@gmail.com>
2025-05-12 01:59:17 +0300

    fix tester

--- settings.py
+++ settings.py
@@ -31,6 +31,36 @@ DEFAULT_TRAIN_CONF = {
     }
 }
 
+DEFAULT_TEST_CONF = {
+    "augs_per_img": [1],
+    "GenerativeModel": {
+        "target_image_size": [224],
+        "g_feature_maps": [32],
+        "d_feature_maps": [32],
+        "n_critic": [3],
+        "losses_weights": {
+            "gen": {
+                "adv": [2.0],
+                "bce": [1.0],
+                "l1": [1.5]
+            },
+            "discr": {
+                "gp": [10.0]
+            }
+        },
+        "optimization_params": {
+            "metric": "iou",
+            "mode": "max",
+            "lr": [0.0005]
+        }
+    },
+    "GANTrainer": {
+        "epochs": [100],
+        "batch_size": [3],
+        "val_ratio": [0.2]
+    }
+} 
+
 # путь к файлу с геоанализом исходных снимков
 GEODATA_PATH = "./data/geo_data.csv"
 
